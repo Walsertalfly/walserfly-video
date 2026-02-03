d712261d84ecf74212b6c3ab8f1370212cdfc363
Walsertalfly <soellerhaus@gmail.com>
2026-02-03 18:33:26 +0100

    Update season covers with empty placeholders

--- season-covers.js
+++ season-covers.js
@@ -1,5 +1,9 @@
 // Auto-generiert von sync-vimeo.py
-// Letzte Aktualisierung: 2026-02-03T16:05:43.383933
+// Wird automatisch vom GitHub Action befüllt
 window.SEASON_COVERS = {
-  winter: 'https://i.vimeocdn.com/video/2116879622-6b5770cd1078f83effc438f6753a4297da0c236eb77ce5225a3ca4362227c9fb-d_1920x1080?&r=pad&region=us',
+  winter: '',
+  fruehling: '',
+  sommer: '',
+  herbst: '',
+  ki: '',
 };
